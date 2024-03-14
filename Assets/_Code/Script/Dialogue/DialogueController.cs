5057488beb286dcdde6b0b0ba269674eaf583584
NakaVostro <ra00297288@pucsp.edu.br>
2024-03-14 19:39:34 -0300

    InputActions

--- Assets/_Code/Script/Dialogue/DialogueController.cs
+++ Assets/_Code/Script/Dialogue/DialogueController.cs
@@ -4,7 +4,6 @@ namespace Paranapiacaba.Dialogue {
     public class DialogueController : MonoSingleton<DialogueController> {
 
         [SerializeField] private float _characterShowDelay;
-        [SerializeField] private Dialogue[] _dialogues;
 
         public void StartDialogue(string dialogueId) {
             Debug.LogWarning("Method Not Implemented Yet");
