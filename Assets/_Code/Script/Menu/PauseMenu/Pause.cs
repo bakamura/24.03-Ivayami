8a6d4a7be78b6c642d7d4b9ed874a6220155617a
NakaVostro <ra00297288@pucsp.edu.br>
2024-04-04 13:27:26 -0300

    Pause Menu Start

--- Assets/_Code/Script/Menu/PauseMenu/Pause.cs
+++ Assets/_Code/Script/Menu/PauseMenu/Pause.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 namespace Paranapiacaba.UI {
     public class Pause : MonoSingleton<Pause> {
 
+        [Header("Events")]
+
         public UnityEvent<bool> onPause = new UnityEvent<bool>();
 
+        [Header("Inputs")]
+
+        [SerializeField] private InputActionReference _pauseInput;
+        [SerializeField] private InputActionReference _unpauseInput;
+
+        private void Start() {
+            _pauseInput.action.started += (callBackContext) => PauseGame(true);
+            _unpauseInput.action.started += (callBackContext) => PauseGame(true);
+        }
+
         public void PauseGame(bool isPausing) {
-            Debug.LogWarning("Method Not Implemented Yet");
+            onPause?.Invoke(isPausing);
         }
 
     }
