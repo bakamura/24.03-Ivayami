f81ead311d8b69c289d031e879be74216327e9ee
NakaVostro <ra00297288@pucsp.edu.br>
2024-04-18 17:43:32 -0300

    Amend

--- Assets/_Code/Script/Menu/PauseMenu/Bag.cs
+++ Assets/_Code/Script/Menu/PauseMenu/Bag.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
+using Paranapiacaba.Player;
 
 namespace Paranapiacaba.UI {
     public class Bag : MonoBehaviour {
+
+        [SerializeField] private Button _itemBtnPrefab;
+        [SerializeField] private Transform _itemSpecialSection;
+        [SerializeField] private Transform _itemGeneralSection;
+        [SerializeField] private Image _itemFocusPreview;
+
+        public void InventoryUpdate() {
+            InventoryItem[] items = PlayerInventory.Instance.CheckInventory();
+            foreach (InventoryItem item in items) {
+                Instantiate(null, item.type == ItemType.Special ? _itemSpecialSection : _itemGeneralSection);
+            }
+        }
 
         public void FocusItem(byte itemIdInBag) {
             Debug.LogWarning("Method Not Implemented Yet");
