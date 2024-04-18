0c13c7e5f7adf337c4ae57d81821331c479e073b
NakaVostro <ra00297288@pucsp.edu.br>
2024-04-18 17:31:18 -0300

    Pause Menu SubMenus Implementation Start

--- Assets/_Code/Script/Menu/PauseMenu/Map.cs
+++ Assets/_Code/Script/Menu/PauseMenu/Map.cs
@@ -1,9 +1,18 @@
+using Paranapiacaba.Scene;
 using UnityEngine;
 
 namespace Paranapiacaba.UI {
     public class Map : MonoBehaviour {
 
+        [SerializeField] private RectTransform _playerPointer;
+        [SerializeField] private RectTransform _goalPointer;
 
+        public void PointersUpdate() {
+            _playerPointer.anchoredPosition = Vector2.zero;
+            _playerPointer.rotation = Quaternion.Euler(0f, 0f, 1f);
+
+            _goalPointer.anchoredPosition = SceneController.Instance.PointerInChapter(0, 0);
+        }
 
     }
 }
