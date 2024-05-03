302720cf59306cc1c2f10cfe83ad30b7cf375a58
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-05-03 18:41:36 -0300

    Revert "unloadAllScenes added + base gameplay scene created"
    This reverts commit 66138f255c46664ca982e2115076b2f68d3b95f5.

--- Assets/_Code/Script/Scene/SceneLoader.cs
+++ Assets/_Code/Script/Scene/SceneLoader.cs
@@ -34,11 +34,6 @@ namespace Ivayami.Scene
             SceneController.Instance.StartLoad(_sceneId, _onSceneLoad);
         }
 
-        public void UnloadAllScenes()
-        {
-            SceneController.Instance.UnloadAllScenes();
-        }
-
 #if UNITY_EDITOR
         private void OnDrawGizmos()
         {
