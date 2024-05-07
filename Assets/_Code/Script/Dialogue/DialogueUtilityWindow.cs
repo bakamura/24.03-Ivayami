6825fd58b8455b1b65c55ca3b35549c17dbb6dbd
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-05-07 09:01:53 -0300

    start on search logic

--- Assets/_Code/Script/Dialogue/DialogueUtilityWindow.cs
+++ Assets/_Code/Script/Dialogue/DialogueUtilityWindow.cs
@@ -11,6 +11,8 @@ namespace Ivayami.Dialogue
         private Dialogue[] _allDialogues;
         private string _filter;
         private Vector2 _scrollPosition;
+        private Rect _buttonRect;
+        private List<Dialogue> _dialoguesCache = new List<Dialogue>();
 
         [MenuItem("Ivayami/DialogueUtilities")]
         private static void ShowWindow()
@@ -22,16 +24,39 @@ namespace Ivayami.Dialogue
 
         private void OnGUI()
         {
-            _allDialogues = Resources.LoadAll<Dialogue>("Dialogues");
-
             _filter = EditorGUILayout.TextField("Filter", _filter);
 
-            DrawSearchResult();
+            _buttonRect = GUILayoutUtility.GetLastRect();
+            _buttonRect.y += _buttonRect.height * 1.2f;
+            if (!string.IsNullOrEmpty(_filter) && GUI.Button(_buttonRect, "Search"))
+            {
+                DrawSearchResult();
+            }
         }
 
         private void DrawSearchResult()
         {
-            _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+            _allDialogues = Resources.LoadAll<Dialogue>("Dialogues");
+            //Rect temp = GUILayoutUtility.GetLastRect();
+            //show transform using the events
+            //show dialogue assets
+
+            for (int i = 0; i < _allDialogues.Length; i++)
+            {
+                for (int a = 0; a < _allDialogues[i].dialogue.Length; a++)
+                {
+                    if (!string.IsNullOrEmpty(_allDialogues[i].dialogue[a].FilterTags) && _allDialogues[i].dialogue[a].FilterTags.Contains(_filter))
+                    {
+                        _dialoguesCache.Add(_allDialogues[i]);
+                    }
+                }
+            }
+            //temp.y += temp.height * 1.2f;            
+            EditorGUILayout.Foldout(true, GUIContent.none);
+            for(int i = 0; i < _dialoguesCache.Count; i++)
+            {
+                EditorGUILayout.ObjectField(_dialoguesCache[i], typeof(Dialogue), false);
+            }
         }
     }
 }
