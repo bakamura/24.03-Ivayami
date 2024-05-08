f6dd8a2085e7a0c5d63685004a7b0b4c4e4b01d3
NakaVostro <ra00297288@pucsp.edu.br>
2024-05-08 18:50:05 -0300

    SceneTransition Menu Access

--- Assets/_Code/Script/Menu/Transition/SceneTransition.cs
+++ Assets/_Code/Script/Menu/Transition/SceneTransition.cs
@@ -3,17 +3,17 @@ namespace Ivayami.UI {
     public class SceneTransition : MonoSingleton<SceneTransition> {
 
         private MenuGroup _menuGroup;
-        private Menu _transition;
+        public Menu Menu { get; private set; }
 
         private void Start() {
             _menuGroup = GetComponent<MenuGroup>();
-            _transition = GetComponent<Menu>();
-            _menuGroup.CloseCurrentThenOpen(_transition);
+            Menu = GetComponent<Menu>();
+            _menuGroup.CloseCurrentThenOpen(Menu);
         }
 
         public void Transition() {
             Logger.Log(LogType.UI, $"Scene Transition Fade");
-            _menuGroup.CloseCurrentThenOpen(_transition);
+            _menuGroup.CloseCurrentThenOpen(Menu);
         }
 
     }
