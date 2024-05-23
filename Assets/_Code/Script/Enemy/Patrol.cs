f75e67e91023a602a74e18dc4298ee5e98e6f818
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-05-23 18:17:07 -0300

    PoliceOfficer + improved EnemyAnimator + ConeVisionDraw

--- Assets/_Code/Script/Enemy/Patrol.cs
+++ Assets/_Code/Script/Enemy/Patrol.cs
@@ -13,25 +13,36 @@ namespace Ivayami.Enemy
         [Header("Parameters")]
         [SerializeField, Min(0f)] private float _minDetectionRange;
         [SerializeField, Min(0f)] private float _detectionRange;
-        [SerializeField] private float _visionAngle;
+        [SerializeField, Range(0f, 180f)] private float _visionAngle = 60f;
+        [SerializeField] private Vector3 _visionOffset;
         [SerializeField, Min(0f), Tooltip("the complete delay is _tickFrequency + this value")] private float _delayBetweenPatrolPoints;
         [SerializeField, Min(.02f)] private float _tickFrequency = .5f;
         [SerializeField, Min(0f)] private float _stressIncreaseOnTargetDetected;
         [SerializeField, Min(0f)] private float _stressIncreaseWhileChasing;
-        [SerializeField] private LayerMask _playerLayer;
-        [SerializeField] private LayerMask _blockVisionLayer;
+        [SerializeField, Min(0f)] private float _stressIncreaseOnAttackTarget;
         [SerializeField] private bool _startActive;
+        [SerializeField] private bool _goToLastTargetPosition;
+        [SerializeField] private bool _attackTarget;
+        [SerializeField] private bool _loseTargetWhenInBush = true;
+        [SerializeField] private LayerMask _targetLayer;
+        [SerializeField] private LayerMask _blockVisionLayer;
         [SerializeField] private Vector3[] _patrolPoints;
 
+#if UNITY_EDITOR
         [Header("Debug")]
         [SerializeField] private bool _debugLog;
-        [SerializeField] private bool _debugDraw;
-        [SerializeField] private Color _minDistanceAreaColor = Color.red;
-        [SerializeField] private Color _detectionRangeAreaColor = Color.green;
+        [SerializeField] private bool _drawMinDistance;
+        [SerializeField] private Color _minDistanceAreaColor = Color.yellow;
+        [SerializeField] private bool _drawDetectionRange;
+        [SerializeField] private Color _detectionRangeAreaColor = Color.red;
+        [SerializeField] private bool _drawPatrolPoints;
         [SerializeField] private Color _patrolPointsColor = Color.black;
+        [SerializeField, Tooltip("if value in NavMeshAgent is 0, the final distance will be collider radius + 0.2")] private bool _drawStoppingDistance;
+        [SerializeField, Tooltip("if value in NavMeshAgent is 0, the final distance will be collider radius + 0.2")] private Color _stoppingDistanceColor = Color.green;
         [SerializeField, Min(0)] private float _patrolPointRadius = .2f;
-
         private Mesh _FOVMesh;
+#endif
+
         private NavMeshAgent _navMeshAgent;
         private WaitForSeconds _behaviourTickDelay;
         private WaitForSeconds _betweenPatrolPointsDelay;
@@ -41,6 +52,9 @@ namespace Ivayami.Enemy
         private EnemyAnimator _enemyAnimator;
         private EnemySounds _enemySounds;
         private Quaternion _initialRotation;
+        private Vector3 _lastTargetPosition;
+        private float _currentTargetColliderSizeFactor;
+        private Collider[] _hitsCache = new Collider[1];
 
         public bool IsActive { get; private set; }
 
@@ -61,7 +75,7 @@ namespace Ivayami.Enemy
         private void Update()
         {
             _enemyAnimator.Walking(_navMeshAgent.velocity.sqrMagnitude > 0);
-            if (_isChasing)
+            if (_isChasing && _stressIncreaseWhileChasing > 0)
                 PlayerStress.Instance.AddStress(_stressIncreaseWhileChasing * Time.deltaTime);
         }
 
@@ -101,35 +115,64 @@ namespace Ivayami.Enemy
             sbyte indexFactor = 1;
             while (true)
             {
-                if (CheckForTarget(halfVisionAngle))
+                if (!_navMeshAgent.isStopped)
                 {
-                    if (!_isChasing)
+                    if (CheckForTarget(halfVisionAngle))
                     {
-                        _enemySounds.PlaySound(EnemySounds.SoundTypes.TargetDetected);
-                        PlayerStress.Instance.AddStress(_stressIncreaseOnTargetDetected);
-                        _isChasing = true;
-                    }
-                    _navMeshAgent.SetDestination(PlayerMovement.Instance.transform.position);
-                    if (_debugLog) Debug.Log("Chase Player");
-                }
-                else
-                {
-                    _isChasing = false;
-                    _navMeshAgent.SetDestination(_patrolPoints[currentPatrolPointIndex] + _initialPosition);
-                    if (_debugLog) Debug.Log("Patroling");
-                    if (Vector3.Distance(transform.position, _patrolPoints[currentPatrolPointIndex] + _initialPosition) <= _navMeshAgent.stoppingDistance)
-                    {
-                        if (_patrolPoints.Length > 1)
+                        if (!_isChasing)
                         {
-                            yield return _betweenPatrolPointsDelay;
-                            currentPatrolPointIndex = (byte)(currentPatrolPointIndex + indexFactor);
-                            if (currentPatrolPointIndex == _patrolPoints.Length - 1) indexFactor = -1;
-                            else if (currentPatrolPointIndex == 0 && indexFactor == -1) indexFactor = 1;
-                            if (_debugLog) Debug.Log($"Change Patrol Point to {currentPatrolPointIndex}");
+                            _enemySounds.PlaySound(EnemySounds.SoundTypes.TargetDetected);
+                            PlayerStress.Instance.AddStress(_stressIncreaseOnTargetDetected);
+                            _isChasing = true;
+                        }
+                        _navMeshAgent.SetDestination(PlayerMovement.Instance.transform.position);
+                        _lastTargetPosition = PlayerMovement.Instance.transform.position;
+                        if (_debugLog) Debug.Log("Chase Target");
+                        if (_attackTarget && Vector3.Distance(transform.position, _navMeshAgent.destination) <= _navMeshAgent.stoppingDistance + _currentTargetColliderSizeFactor)
+                        {
+                            _navMeshAgent.isStopped = true;
+                            _navMeshAgent.velocity = Vector3.zero;
+                            PlayerStress.Instance.AddStress(_stressIncreaseOnAttackTarget);
+                            _isChasing = false;
+                            _enemyAnimator.Attack(OnAttackAnimationEnd);
+                            if (_debugLog) Debug.Log("Attack Target");
+                        }
+                    }
+                    else
+                    {
+                        if (_isChasing)
+                        {
+                            if (_goToLastTargetPosition)
+                            {
+                                _navMeshAgent.SetDestination(_lastTargetPosition);
+                                if (_debugLog) Debug.Log($"Moving to last target position {_lastTargetPosition}");
+                                if (Vector3.Distance(transform.position, _lastTargetPosition) <= _navMeshAgent.stoppingDistance)
+                                {
+                                    _isChasing = false;
+                                    if (_debugLog) Debug.Log("Last target point reached");
+                                }
+                            }
+                            else _isChasing = false;
                         }
                         else
                         {
-                            transform.rotation = Quaternion.RotateTowards(transform.rotation, _initialRotation, _navMeshAgent.angularSpeed * _tickFrequency);
+                            _navMeshAgent.SetDestination(_patrolPoints[currentPatrolPointIndex] + _initialPosition);
+                            if (_debugLog) Debug.Log("Patroling");
+                            if (Vector3.Distance(transform.position, _patrolPoints[currentPatrolPointIndex] + _initialPosition) <= _navMeshAgent.stoppingDistance)
+                            {
+                                if (_patrolPoints.Length > 1)
+                                {
+                                    yield return _betweenPatrolPointsDelay;
+                                    currentPatrolPointIndex = (byte)(currentPatrolPointIndex + indexFactor);
+                                    if (currentPatrolPointIndex == _patrolPoints.Length - 1) indexFactor = -1;
+                                    else if (currentPatrolPointIndex == 0 && indexFactor == -1) indexFactor = 1;
+                                    if (_debugLog) Debug.Log($"Change Patrol Point to {currentPatrolPointIndex}");
+                                }
+                                else
+                                {
+                                    transform.rotation = Quaternion.RotateTowards(transform.rotation, _initialRotation, _navMeshAgent.angularSpeed * _tickFrequency);
+                                }
+                            }
                         }
                     }
                 }
@@ -139,141 +182,61 @@ namespace Ivayami.Enemy
 
         private bool CheckForTarget(float halfVisionAngle)
         {
-            return !Bush.IsPlayerHidden && Physics.CheckSphere(transform.position, _detectionRange, _playerLayer) &&
-                    !Physics.Raycast(transform.position, (PlayerMovement.Instance.transform.position - transform.position).normalized, _detectionRange, _blockVisionLayer) &&
-                    (Vector3.Distance(PlayerMovement.Instance.transform.position, transform.position) <= _minDetectionRange
-                    || (Vector3.Distance(PlayerMovement.Instance.transform.position, transform.position) <= _detectionRange
-                    && Vector3.Angle(PlayerMovement.Instance.transform.position, transform.position) <= halfVisionAngle));
+            bool isNotInBush = (_loseTargetWhenInBush && !Bush.IsPlayerHidden) || !_loseTargetWhenInBush;
+            bool targetInsideRange = Physics.OverlapSphereNonAlloc(transform.position, _detectionRange, _hitsCache, _targetLayer) > 0;
+            if (targetInsideRange) _currentTargetColliderSizeFactor = _hitsCache[0].bounds.extents.z;
+            bool blockingVision = Physics.Raycast(transform.position + _visionOffset, (PlayerMovement.Instance.transform.position - transform.position).normalized, Vector3.Distance(transform.position, PlayerMovement.Instance.transform.position), _blockVisionLayer);
+            bool isInMinRange = Vector3.Distance(PlayerMovement.Instance.transform.position, transform.position) <= _minDetectionRange;
+            bool isInVisionAngle = Vector3.Angle(transform.forward, (PlayerMovement.Instance.transform.position - transform.position).normalized) <= halfVisionAngle;
+
+            if (_debugLog)
+                Debug.Log($"blocked by bush {!isNotInBush}, target Inside Radius {targetInsideRange}, blocking vision {blockingVision}, is in Min range {isInMinRange}, is in Vision Angle {isInVisionAngle}");
+            return isNotInBush && targetInsideRange && !blockingVision && (isInMinRange || isInVisionAngle);
+        }
+
+        private void OnAttackAnimationEnd()
+        {
+            _navMeshAgent.isStopped = false;
+            Debug.Log("Callback");
         }
         #region Debug
+#if UNITY_EDITOR
         private void OnDrawGizmosSelected()
         {
-            if (_debugDraw)
+            Vector3 pos = _initialPosition != Vector3.zero ? _initialPosition : transform.position;
+            if (_drawDetectionRange)
             {
+                _FOVMesh = DebugUtilities.CreateConeMesh(transform, _visionAngle, _detectionRange);
                 Gizmos.color = _detectionRangeAreaColor;
-                Gizmos.DrawMesh(_FOVMesh, transform.position, transform.rotation);
-
+                Gizmos.DrawMesh(_FOVMesh, transform.position + _visionOffset, Quaternion.identity);
+            }
+            if (_drawMinDistance)
+            {
                 Gizmos.color = _minDistanceAreaColor;
                 Gizmos.DrawSphere(transform.position, _minDetectionRange);
-
+            }
+            if (_drawPatrolPoints)
+            {
                 Gizmos.color = _patrolPointsColor;
-                for (int i = 0; i < _patrolPoints.Length; i++)
+                int i;
+                for (i = 0; i < _patrolPoints.Length; i++)
                 {
-                    Vector3 pos = _initialPosition != Vector3.zero ? _initialPosition : transform.position;
                     Gizmos.DrawSphere(pos + _patrolPoints[i], _patrolPointRadius);
+                    UnityEditor.Handles.Label(pos + _patrolPoints[i] + new Vector3(0, _patrolPointRadius * 2, 0), i.ToString());
+                }
+                for (i = 0; i < _patrolPoints.Length - 1; i++)
+                {
+                    Gizmos.DrawLine(pos + _patrolPoints[i], pos + _patrolPoints[Mathf.Clamp(i + 1, 0, _patrolPoints.Length - 1)]);
                 }
             }
+            if (_drawStoppingDistance)
+            {
+                if (!_navMeshAgent) _navMeshAgent = GetComponent<NavMeshAgent>();
+                Gizmos.color = _stoppingDistanceColor;
+                Gizmos.DrawLine(pos, pos + transform.right * _navMeshAgent.stoppingDistance);
+            }
         }
-
-        private void OnValidate()
-        {
-            _FOVMesh = CreateConeMesh();
-        }
-
-        //creates the visual cone in the editor thar represents the FOV
-        private Mesh CreateConeMesh()
-        {
-            Mesh pyramid = new Mesh();
-            int numOfTriangles = 14;
-            int numVertices = numOfTriangles * 3;
-            float halfAngle = _visionAngle / 2f;
-            Vector3[] vertices = new Vector3[numVertices];
-            int[] triangles = new int[numVertices];
-
-            Vector3 Center = transform.forward;//0 
-
-            Vector3 Left = Quaternion.Euler(0, -halfAngle, 0) * transform.forward * _detectionRange;
-            Vector3 Right = Quaternion.Euler(0, halfAngle, 0) * transform.forward * _detectionRange;
-
-            Vector3 top = Quaternion.Euler(-halfAngle, 0, 0) * transform.forward * _detectionRange;
-            Vector3 bottom = Quaternion.Euler(halfAngle, 0, 0) * transform.forward * _detectionRange;
-
-            float middlePointAngles = halfAngle * .7f;
-
-            Vector3 MiddleTopLeft = Quaternion.Euler(-middlePointAngles, -middlePointAngles, 0) * transform.forward * _detectionRange;
-            Vector3 MiddleTopRight = Quaternion.Euler(-middlePointAngles, middlePointAngles, 0) * transform.forward * _detectionRange;
-
-            Vector3 MiddleBottomLeft = Quaternion.Euler(middlePointAngles, -middlePointAngles, 0) * transform.forward * _detectionRange;
-            Vector3 MiddleBottomRight = Quaternion.Euler(middlePointAngles, middlePointAngles, 0) * transform.forward * _detectionRange;
-
-            int currentVert = 0;
-
-            //top left
-            vertices[currentVert++] = top;
-            vertices[currentVert++] = Center;
-            vertices[currentVert++] = MiddleTopLeft;
-
-            //middle top left
-            vertices[currentVert++] = MiddleTopLeft;
-            vertices[currentVert++] = Center;
-            vertices[currentVert++] = Left;
-
-            //middle bottom left
-            vertices[currentVert++] = Left;
-            vertices[currentVert++] = Center;
-            vertices[currentVert++] = MiddleBottomLeft;
-
-            //bottom left
-            vertices[currentVert++] = MiddleBottomLeft;
-            vertices[currentVert++] = Center;
-            vertices[currentVert++] = bottom;
-
-            //bottom right
-            vertices[currentVert++] = bottom;
-            vertices[currentVert++] = Center;
-            vertices[currentVert++] = MiddleBottomRight;
-
-            //middle bottom right
-            vertices[currentVert++] = MiddleBottomRight;
-            vertices[currentVert++] = Center;
-            vertices[currentVert++] = Right;
-
-            //middle top right
-            vertices[currentVert++] = Right;
-            vertices[currentVert++] = Center;
-            vertices[currentVert++] = MiddleTopRight;
-
-            //top right
-            vertices[currentVert++] = MiddleTopRight;
-            vertices[currentVert++] = Center;
-            vertices[currentVert++] = top;
-
-            //pyramid base
-            //top
-            vertices[currentVert++] = top;
-            vertices[currentVert++] = MiddleTopLeft;
-            vertices[currentVert++] = MiddleTopRight;
-
-            //top middle
-            vertices[currentVert++] = MiddleTopRight;
-            vertices[currentVert++] = MiddleTopLeft;
-            vertices[currentVert++] = Right;
-
-            vertices[currentVert++] = Right;
-            vertices[currentVert++] = MiddleTopLeft;
-            vertices[currentVert++] = Left;
-
-            //bottom middle
-            vertices[currentVert++] = Left;
-            vertices[currentVert++] = MiddleBottomRight;
-            vertices[currentVert++] = Right;
-
-            vertices[currentVert++] = MiddleBottomRight;
-            vertices[currentVert++] = Left;
-            vertices[currentVert++] = MiddleBottomLeft;
-
-            //bottom
-            vertices[currentVert++] = MiddleBottomLeft;
-            vertices[currentVert++] = bottom;
-            vertices[currentVert++] = MiddleBottomRight;
-
-            for (int i = 0; i < numVertices; i++) triangles[i] = i;
-            pyramid.vertices = vertices;
-            pyramid.triangles = triangles;
-            pyramid.RecalculateNormals();
-
-            return pyramid;
-        }
+#endif
         #endregion
     }
 }
