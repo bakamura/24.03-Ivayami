1a4089e57802624662f49a3041e7371eb90913ec
NakaVostro <ra00297288@pucsp.edu.br>
2024-06-07 00:05:14 -0300

    UI Language Start

---
+++ Assets/_Code/Script/Menu/MainMenu/UiText.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ivayami.UI {
+    [CreateAssetMenu(menuName = "Texts/UI")]
+    public class UiText : ScriptableObject {
+
+        [SerializeField] private string[] _keys;
+        [SerializeField] private string[] _values;
+        private Dictionary<string, string> _dictionary;
+
+        public string GetText(string key) {
+            if(_dictionary == null) InitDict();
+            return _dictionary[key];
+        }
+
+        private void InitDict() {
+            if (_keys != null && _keys.Length > 0) {
+                _dictionary = new Dictionary<string, string>();
+                for (int i = 0; i < _keys.Length; i++) _dictionary.Add(_keys[i], _values[i]);
+                _keys = null;
+                _values = null;
+            }
+            else {
+                Debug.LogError($"Could not initialize UiText with null Key/Values '{name}'");
+            }
+        }
+
+    }
+}
--- Assets/_Code/Script/Puzzle/Readable.cs
+++ Assets/_Code/Script/Puzzle/Readable.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 namespace Ivayami.Puzzle {
-    [CreateAssetMenu(menuName = "Item/Readable")]
+    [CreateAssetMenu(menuName = "Texts/Readable")]
     public class Readable : ScriptableObject {
 
         [field: SerializeField, TextArea(1, 50)] public string Content { get; private set; }
