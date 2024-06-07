68c0eabcd27c354ba914d646b4261f93dcfbe9d9
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-06-07 08:48:54 -0300

    amend to previous

--- Assets/_Code/Script/Puzzle/Activable.cs
+++ Assets/_Code/Script/Puzzle/Activable.cs
@@ -4,7 +4,7 @@ namespace Ivayami.Puzzle
 {
     public class Activable : MonoBehaviour
     {
-        [SerializeField] protected byte _activatosNeededToActivate;
+        [SerializeField, Min(0), Tooltip("if 0 will require all activators in list to activate")] protected int _activatosNeededToActivate;
         [SerializeField] private Activator[] _activators;
         [HideInInspector] public bool IsActive { get; protected set; }
 
@@ -14,7 +14,7 @@ namespace Ivayami.Puzzle
             {
                 _activators[i].onActivate.AddListener(HandleOnActivate);
             }
-            if (_activatosNeededToActivate == 0) _activatosNeededToActivate = (byte)_activators.Length;
+            if (_activatosNeededToActivate == 0) _activatosNeededToActivate = _activators.Length;
         }
 
         protected virtual void HandleOnActivate()
@@ -27,5 +27,12 @@ namespace Ivayami.Puzzle
             }
             IsActive = currentActiveAmount >= _activatosNeededToActivate;
         }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_activatosNeededToActivate > _activators.Length) _activatosNeededToActivate = _activators.Length;
+        }
+#endif
     }
 }
