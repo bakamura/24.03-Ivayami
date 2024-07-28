975c0f7d29874510e387e0a47f30a48a25e2777d
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-07-28 15:27:16 -0300

    fixes to UiLanguage

--- Assets/_Code/Script/Menu/UiLanguage.cs
+++ Assets/_Code/Script/Menu/UiLanguage.cs
@@ -2,41 +2,54 @@ using System;
 using UnityEngine;
 using TMPro;
 using Ivayami.Save;
-using Ivayami.UI;
 
-public class UiLanguage : MonoBehaviour {
+namespace Ivayami.UI
+{
+    public class UiLanguage : MonoBehaviour
+    {
+        [Serializable]
+        public class LanguageComponent
+        {
+            [field: SerializeField] public TextMeshProUGUI Tmp { get; private set; }
+            public string Id;
+        }
 
-    [System.Serializable]
-    public class LanguageComponent {
-        [field: SerializeField] public TextMeshProUGUI Tmp { get; private set; }
-        public string Id;
-    }
+        [Header("UI")]
 
-    [Header("UI")]
+        [SerializeField] private UiText _uiText;
+        [SerializeField] private LanguageComponent[] _languageComponents;
 
-    [SerializeField] private UiText _uiText;
-    [SerializeField] private LanguageComponent[] _languageComponents;
+        private void Start()
+        {
+            Options.OnChangeLanguage.AddListener(UpdateLanguage);
+            if (SaveSystem.Instance && SaveSystem.Instance.Options != null) UpdateLanguage((LanguageTypes)SaveSystem.Instance.Options.language);
+        }
 
-    private void Start() {
-        Options.OnChangeLanguage.AddListener(UpdateLanguage);
-        if(SaveSystem.Instance.Options != null) UpdateLanguage((LanguageTypes) SaveSystem.Instance.Options.language);
-    }
+        private void OnDestroy()
+        {
+            Options.OnChangeLanguage.RemoveListener(UpdateLanguage);
+        }
 
-    public void UpdateLanguage(LanguageTypes language) {
-        UiText uiText = _uiText.GetTranslation(language);
-        foreach(LanguageComponent languageComponent in _languageComponents) languageComponent.Tmp.text = uiText.GetText(languageComponent.Id);
-    }
+        public void UpdateLanguage(LanguageTypes language)
+        {
+            UiText uiText = _uiText.GetTranslation(language);
+            foreach (LanguageComponent languageComponent in _languageComponents) languageComponent.Tmp.text = uiText.GetText(languageComponent.Id);
+        }
 
-    [ContextMenu("Set To UI Text")]
-    private void SetToUiText() {
-        if (_uiText != null) {
-            if (_languageComponents == null) _languageComponents = new LanguageComponent[_uiText.Size];
-            else Array.Resize(ref _languageComponents, _uiText.Size);
-            for (int i = 0; i < _uiText.Size; i++) {
-                if (_languageComponents[i] == null) _languageComponents[i] = new LanguageComponent();
-                _languageComponents[i].Id = _uiText.Keys[i];
+        [ContextMenu("Set To UI Text")]
+        private void SetToUiText()
+        {
+            if (_uiText != null)
+            {
+                if (_languageComponents == null) _languageComponents = new LanguageComponent[_uiText.Size];
+                else Array.Resize(ref _languageComponents, _uiText.Size);
+                for (int i = 0; i < _uiText.Size; i++)
+                {
+                    if (_languageComponents[i] == null) _languageComponents[i] = new LanguageComponent();
+                    _languageComponents[i].Id = _uiText.Keys[i];
+                }
             }
         }
-    }
 
+    }
 }
