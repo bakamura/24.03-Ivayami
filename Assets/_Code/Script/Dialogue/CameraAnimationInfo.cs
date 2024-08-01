02587a1511ecba7cc819217028aa5e5a258bde8c
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-08-01 19:08:42 -0300

    Hide Visual option added to CameraAnimationInfo

--- Assets/_Code/Script/Dialogue/CameraAnimationInfo.cs
+++ Assets/_Code/Script/Dialogue/CameraAnimationInfo.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
 using UnityEngine.Serialization;
+using Ivayami.Player;
 
 namespace Ivayami.Dialogue
 {
     public class CameraAnimationInfo : MonoBehaviour
     {
         [FormerlySerializedAs("duration"), Min(0)] public float Duration;
+        [SerializeField] private bool _hidePlayerModel;
         [FormerlySerializedAs("positionCurve")] public AnimationCurve PositionCurve;
         [FormerlySerializedAs("rotationCurve")] public AnimationCurve RotationCurve;
 
         public void StartMovement()
         {
+            if(_hidePlayerModel) PlayerMovement.Instance.UpdateVisualsVisibility(true);
             DialogueCamera.Instance.MoveRotate(this);
         }
 
         public void ExitDialogueCamera()
         {
+            if (_hidePlayerModel) PlayerMovement.Instance.UpdateVisualsVisibility(false);
             DialogueCamera.Instance.ExitDialogeCamera();
         }
     }
