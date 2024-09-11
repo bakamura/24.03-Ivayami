5445a045955af9f6da39bf97f2977ad945206c57
NakaVostro <ra00297288@pucsp.edu.br>
2024-09-11 15:02:26 -0300

    Stress Relieve Area

---
+++ Assets/_Code/Script/Scene/StressRelieveArea.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using Ivayami.Player;
+
+namespace Ivayami.Scene {
+    public class StressRelieveArea : MonoBehaviour {
+
+        [Header("Parameters")]
+
+        [SerializeField, Min(0f)] private float _stressRelieveRate;
+        private Coroutine _stressRelieveRoutine;
+        [SerializeField, Min(0f] private float _routineTickRate;
+
+        private void OnTriggerEnter(Collider other) {
+            if(other.transform.name.Equals(PlayerMovement.Instance.name)) _stressRelieveRoutine = StartCoroutine(RelieveRoutine());
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (other.transform.name.Equals(PlayerMovement.Instance.name) && _stressRelieveRoutine != null) StopCoroutine(_stressRelieveRoutine);
+        }
+
+        private void OnDestroy() {
+            if (_stressRelieveRoutine != null) StopCoroutine(_stressRelieveRoutine);
+        }
+
+        private IEnumerator RelieveRoutine() {
+            WaitForSeconds tick = new WaitForSeconds(_routineTickRate);
+            float relieveTick = -_stressRelieveRate / _routineTickRate;
+            while (true) {
+                PlayerStress.Instance.AddStress(relieveTick * Time.deltaTime);
+
+                yield return tick;
+            }
+
+        }
+
+    }
+}
