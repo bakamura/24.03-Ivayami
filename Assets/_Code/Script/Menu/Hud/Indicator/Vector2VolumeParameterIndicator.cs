460425f9eea52fff8f54ecd8a84d053ead1472e4
NakaVostro <ra00297288@pucsp.edu.br>
2024-10-08 17:40:37 -0300

    Make lens warping

--- Assets/_Code/Script/Menu/Hud/Indicator/Vector2VolumeParameterIndicator.cs
+++ Assets/_Code/Script/Menu/Hud/Indicator/Vector2VolumeParameterIndicator.cs
@@ -4,9 +4,13 @@ using UnityEngine.Rendering;
 namespace Ivayami.UI {
     public class Vector2VolumeParameterIndicator : VolumeParameterIndicator<Vector2Parameter, Vector2> {
 
+        [Tooltip("Leave at 0 for linear transition")]
+        [SerializeField] private float _spinRate;
+
         private float _circleLength;
         private float _angleCache;
         private Vector2 _vectorCache;
+        private Vector2 _vectorHalf = Vector2.one / 2f;
 
         protected override void Awake() {
             base.Awake();
@@ -15,13 +19,17 @@ namespace Ivayami.UI {
         }
 
         public override void FillUpdate(float value) {
-            _volumeParameter.value = ValueToVector2(EvaluateCurve(value));
+            _volumeParameter.value = _spinRate > 0 ? AngleToVector2Spin(EvaluateCurve(value)) : Vector2.Lerp(_valueMin, _valueMax, EvaluateCurve(value));
         }
 
-        private Vector2 ValueToVector2(float value) {
-            _angleCache = value * _circleLength;
-            _vectorCache[0] = Mathf.Cos(value);
-            _vectorCache[1] = Mathf.Sin(value);
+        private Vector2 AngleToVector2Spin(float value) {
+            if (value <= 0) return _vectorHalf;
+            _angleCache += value * _spinRate * Time.deltaTime;
+            if (_angleCache > 360f) _angleCache -= 360f;
+            _vectorCache = Vector2.Lerp(_valueMin, _valueMax, value);
+            _vectorCache[0] *= Mathf.Cos(_angleCache) / 2f;
+            _vectorCache[1] *= Mathf.Sin(_angleCache) / 2f;
+            _vectorCache += _vectorHalf;
             return _vectorCache;
         }
 
