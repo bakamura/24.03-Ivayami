74d7f6eda978f2ba2bbbfa4d6643083f8f45d40e
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-10-29 20:57:10 -0300

    HitboxAttack optimization

--- Assets/_Code/Script/Enemy/ComponentsAndInterfaces/HitboxAttack.cs
+++ Assets/_Code/Script/Enemy/ComponentsAndInterfaces/HitboxAttack.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Ivayami.Player;
 using UnityEngine.Events;
+using System.Collections;
 
 namespace Ivayami.Enemy
 {
@@ -22,6 +23,9 @@ namespace Ivayami.Enemy
         private float _currentStressIncreaseOnEnter;
         private float _currentStressIncreaseOnStay;
         private bool _previousState;
+        private const float _tickFrequency = .5f;
+        private bool _isTargetInside;
+        private Coroutine _damageCoroutine;
 
         public void UpdateHitbox(bool isActive, Vector3 center, Vector3 size, float stressIncreaseOnEnter, float stressIncreaseOnStay)
         {
@@ -30,20 +34,34 @@ namespace Ivayami.Enemy
             _currentStressIncreaseOnEnter = stressIncreaseOnEnter;
             _currentStressIncreaseOnStay = stressIncreaseOnStay;
             _boxCollider.enabled = isActive;
+            if (!isActive) _isTargetInside = false;
             if (!_previousState && isActive) OnHitboxActivate?.Invoke();
             if (_previousState && !isActive) OnHitboxDeactivate?.Invoke();
             _previousState = isActive;
         }
 
+        private IEnumerator DamageCoroutine()
+        {
+            WaitForSeconds delay = new WaitForSeconds(_tickFrequency);
+            while (_isTargetInside)
+            {
+                PlayerStress.Instance.AddStress(_currentStressIncreaseOnStay);
+                yield return delay;
+            }
+            _damageCoroutine = null;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
+            _isTargetInside = true;
             if (_currentStressIncreaseOnEnter > 0) PlayerStress.Instance.AddStress(_currentStressIncreaseOnEnter);
+            if (_currentStressIncreaseOnStay > 0 && _damageCoroutine == null) _damageCoroutine = StartCoroutine(DamageCoroutine());            
             OnTargetHit?.Invoke();
         }
 
-        private void OnTriggerStay(Collider other)
+        private void OnTriggerExit(Collider other)
         {
-            if (_currentStressIncreaseOnStay > 0) PlayerStress.Instance.AddStress(_currentStressIncreaseOnStay);
+            _isTargetInside = false;
         }
     }
 }
