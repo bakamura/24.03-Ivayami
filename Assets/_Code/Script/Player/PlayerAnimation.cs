c4252dcd259d2a13bf6b9ddb00b0d8ec7e92f6ea
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-11-02 22:32:08 -0300

    amend

--- Assets/_Code/Script/Player/PlayerAnimation.cs
+++ Assets/_Code/Script/Player/PlayerAnimation.cs
@@ -56,7 +56,6 @@ namespace Ivayami.Player {
         }
 
         private void Crouch(bool isCrouching) {
-            Debug.Log("CROUNCH");
             _animator.SetBool(CROUCH, isCrouching);
         }
 
