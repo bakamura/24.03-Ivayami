5cc145fcb5d7fb8cc3a79e5a4952633afd35e46c
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-11-11 22:17:26 -0300

    InteractableObjectGroup + deliverUI and Hiding spot return with interaction btn

--- Assets/_Code/Script/Puzzle/Misc/HidingSpot.cs
+++ Assets/_Code/Script/Puzzle/Misc/HidingSpot.cs
@@ -27,6 +27,7 @@ namespace Ivayami.Puzzle {
         private Animator _objectAnimator;
         private Coroutine _hideCoroutine;
         private const string BLOCK_KEY = "hidingSpot";
+        private bool _inputActive;
 
         private void Awake() {
             InteratctableFeedbacks = GetComponent<InteractableFeedbacks>();
@@ -52,7 +53,7 @@ namespace Ivayami.Puzzle {
         private IEnumerator HideRoutine() {
             PlayerStress.Instance.onFail.AddListener(OnPlayerDeath);
             PlayerActions.Instance.ChangeInputMap("Menu");
-            Pause.Instance.ToggleCanPause(BLOCK_KEY, false);
+            Pause.Instance.ToggleCanPause(BLOCK_KEY, false);            
             _hidingCam.StartMovement();
             PlayerMovement.Instance.SetPosition(_animationPoint.position);
             PlayerMovement.Instance.SetTargetAngle(_animationPoint.eulerAngles.y);
@@ -66,11 +67,19 @@ namespace Ivayami.Puzzle {
             PlayerMovement.Instance.hidingState = _hiddenType; 
             ReturnAction.Instance.Set(Exit);
             _exitInput.action.started += HandleExit;
+            _exitInput.action.Enable();
+            PlayerMovement.Instance.ToggleMovement(nameof(HidingSpot), false);
+            _inputActive = true;
             _hideCoroutine = null;
         }        
 
         public void Exit() {
-            _exitInput.action.started -= HandleExit;
+            if (_inputActive)
+            {
+                _exitInput.action.started -= HandleExit;
+                PlayerMovement.Instance.ToggleMovement(nameof(HidingSpot), true);
+            }
+            _inputActive = false;
             PlayerStress.Instance.onFail.RemoveListener(OnPlayerDeath);
             PlayerMovement.Instance.hidingState = PlayerMovement.HidingState.None;
             PlayerActions.Instance.ChangeInputMap("Player");
@@ -83,6 +92,12 @@ namespace Ivayami.Puzzle {
 
         private void OnPlayerDeath()
         {
+            if (_inputActive)
+            {
+                _exitInput.action.started -= HandleExit;
+                PlayerMovement.Instance.ToggleMovement(nameof(HidingSpot), true);
+            }
+            _inputActive = false;
             PlayerStress.Instance.onFail.RemoveListener(OnPlayerDeath);
             _objectAnimator.speed = 0;
             PlayerMovement.Instance.hidingState = PlayerMovement.HidingState.None;
