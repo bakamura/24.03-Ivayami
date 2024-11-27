54accbc7e4814616b620ff2961d2d834aee914a7
Pedro Nakamura <ra00297288@pucsp.edu.br>
2024-11-27 15:56:28 -0300

    Logic + Logs added

--- Assets/_Code/Script/InputCallbacks.cs
+++ Assets/_Code/Script/InputCallbacks.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
@@ -8,14 +9,24 @@ namespace Ivayami.Player {
 
         private PlayerInput _playerInput;
         public bool IsGamepad { get { return _playerInput.currentControlScheme == "Gamepad"; } }
+        public GamepadType GamepadCurrent { get; private set; }
+
+        public enum GamepadType {
+            NotGamepad,
+            XBox,
+            DualShock,
+            DualSense
+        }
 
         protected override void Awake() {
             base.Awake();
 
             _playerInput = GetComponent<PlayerInput>();
-            _playerInput.onControlsChanged += (playerInput) => _onChangeControls.Invoke(playerInput.currentControlScheme == "Gamepad");
+            _playerInput.onControlsChanged += (playerInput) => {
+                GamepadCurrent = IsGamepad ? StringToGamepadType(Gamepad.current.displayName) : GamepadType.NotGamepad;
+                _onChangeControls.Invoke(playerInput.currentControlScheme == "Gamepad");
+            };
         }
-
 
         /// <summary>
         /// Callback 'true' when 'isGamepad == true'
@@ -29,72 +40,15 @@ namespace Ivayami.Player {
             _onChangeControls.RemoveListener(action);
         }
 
-        #region Old Version
-        // Subscribe to this event
-        //public event Action<ControlScheme> OnInputSchemeChanged;
-        //public ControlScheme CurrentControlScheme { get; private set; }
-        //public InputActionAsset inputActions;
+        public GamepadType StringToGamepadType(string str) {
+            str = str.ToLower();
+            Debug.Log($"Gamepad named '{str}' connected");
+            if (str.Contains("xbox")) return GamepadType.XBox;
+            if (str.Contains("dualshock")) return GamepadType.DualShock;
+            if (str.Contains("dualsense")) return GamepadType.DualSense;
+            Debug.LogError($"Non-identified Gamepad '{str}'");
+            return GamepadType.XBox;
+        }
 
-        //public InputUser user;
-        //private void Start() => StartAutoControlSchemeSwitching();
-        //private void OnDestroy() => StopAutoControlSchemeSwitching();        
-
-        //void StartAutoControlSchemeSwitching()
-        //{
-        //    user = InputUser.CreateUserWithoutPairedDevices();
-        //    user.AssociateActionsWithUser(inputActions.actionMaps[0]); // need to be there at least one actionmap defined in InputActionAsset, otherwise rises exception during paring process
-        //    ++InputUser.listenForUnpairedDeviceActivity;
-        //    InputUser.onUnpairedDeviceUsed += InputUser_onUnpairedDeviceUsed;
-        //    user.UnpairDevices();
-        //}
-
-        //private void StopAutoControlSchemeSwitching()
-        //{
-        //    InputUser.onUnpairedDeviceUsed -= InputUser_onUnpairedDeviceUsed;
-        //    if (InputUser.listenForUnpairedDeviceActivity > 0)
-        //        --InputUser.listenForUnpairedDeviceActivity;
-        //    user.UnpairDevicesAndRemoveUser();
-        //}
-
-        //private void InputUser_onUnpairedDeviceUsed(InputControl ctrl, UnityEngine.InputSystem.LowLevel.InputEventPtr eventPtr)
-        //{
-        //    var device = ctrl.device;
-
-        //    if ((CurrentControlScheme == ControlScheme.KeyboardMouse) &&
-        //         ((device is Pointer) || (device is Keyboard)))
-        //    {
-        //        InputUser.PerformPairingWithDevice(device, user);
-        //        if (OnInputSchemeChanged != null) OnInputSchemeChanged(ControlScheme.KeyboardMouse);
-        //        SetUserControlScheme(ControlScheme.KeyboardMouse);
-        //        return;
-        //    }
-
-        //    if (device is Gamepad)
-        //    {
-        //        if (OnInputSchemeChanged != null) OnInputSchemeChanged(ControlScheme.Gamepad);
-        //        CurrentControlScheme = ControlScheme.Gamepad;
-        //        SetUserControlScheme(ControlScheme.Gamepad);
-        //    }
-        //    else if ((device is Keyboard) || (device is Pointer))
-        //    {
-        //        if (OnInputSchemeChanged != null) OnInputSchemeChanged(ControlScheme.KeyboardMouse);
-        //        CurrentControlScheme = ControlScheme.KeyboardMouse;
-        //        SetUserControlScheme(ControlScheme.KeyboardMouse);
-        //    }
-        //    else return;
-
-        //    user.UnpairDevices();
-        //    InputUser.PerformPairingWithDevice(device, user);
-        //}
-        //public void SetUserControlScheme(ControlScheme scheme)
-        //{
-        //    //user.ActivateControlScheme(scheme.ToString());
-        //    user.ActivateControlScheme(inputActions.controlSchemes[(int)scheme]); // this should be faster and not vulnerable to scheme string names
-        //}
-        //public enum ControlScheme
-        //{
-        //    KeyboardMouse = 0, Gamepad = 1 // just need to be same indexes as defined in inputActionAsset
-        //}
-        #endregion
     }
 }
