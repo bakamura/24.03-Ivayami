bcb5f3102a888dce5e6336a806dba975b0c1e950
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2024-11-30 15:32:31 -0300

    Revert "Stress Relieve Adjustable start"
    This reverts commit ba1db1b52ac7a79204418aa2f39fa83179371fda.

--- Assets/_Code/Script/Save/ProgressTriggerEvent.cs
+++ Assets/_Code/Script/Save/ProgressTriggerEvent.cs
@@ -3,17 +3,22 @@ using System;
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace Ivayami.Save {
-    public class ProgressTriggerEvent : MonoBehaviour {
+namespace Ivayami.Save
+{
+    //[RequireComponent(typeof(BoxCollider))]
+    public class ProgressTriggerEvent : MonoBehaviour
+    {
 
         [SerializeField] private bool _triggerOnStart;
         [Serializable]
-        public struct ProgressConditionInfo {
+        public struct ProgressConditionInfo
+        {
             public AreaProgress AreaProgress;
             public int ProgressStepMin;
             public int ProgressStepMax;
 
-            public ProgressConditionInfo(AreaProgress areaProgress, int min, int max) {
+            public ProgressConditionInfo(AreaProgress areaProgress, int min, int max)
+            {
                 AreaProgress = areaProgress;
                 ProgressStepMin = min;
                 ProgressStepMax = max;
@@ -22,24 +27,31 @@ namespace Ivayami.Save {
         [SerializeField, ProgressStepAttribute] private ProgressConditionInfo[] _progressConditions;
         [SerializeField] private UnityEvent _onTrigger;
 
-        private void Start() {
+        private void Start()
+        {
             if (_triggerOnStart) TryTrigger();
         }
 
-        private void OnTriggerEnter(Collider other) {
+        private void OnTriggerEnter(Collider other)
+        {
             TryTrigger();
         }
 
-        public void TryTrigger() {
+        public void TryTrigger()
+        {
             if (!SaveSystem.Instance || SaveSystem.Instance.Progress == null) return;
-            for (int i = 0; i < _progressConditions.Length; i++) {
-                if (!SaveSystem.Instance.Progress.gameProgress.ContainsKey(_progressConditions[i].AreaProgress.Id)) continue;
-                else if (SaveSystem.Instance.Progress.gameProgress[_progressConditions[i].AreaProgress.Id] < _progressConditions[i].ProgressStepMin ||
-                         SaveSystem.Instance.Progress.gameProgress[_progressConditions[i].AreaProgress.Id] > _progressConditions[i].ProgressStepMax) continue;
-                _onTrigger?.Invoke();
+            for (int i = 0; i < _progressConditions.Length; i++)
+            {
+                if (!SaveSystem.Instance.Progress.gameProgress.ContainsKey(_progressConditions[i].AreaProgress.Id) ||
+                    (SaveSystem.Instance.Progress.gameProgress.ContainsKey(_progressConditions[i].AreaProgress.Id) &&
+                    (SaveSystem.Instance.Progress.gameProgress[_progressConditions[i].AreaProgress.Id] < _progressConditions[i].ProgressStepMin ||
+                    SaveSystem.Instance.Progress.gameProgress[_progressConditions[i].AreaProgress.Id] > _progressConditions[i].ProgressStepMax)))
+                {
+                    return;
+                }
             }
+            _onTrigger?.Invoke();
         }
+
     }
-
 }
-
