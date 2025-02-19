801f47cb2ab6b9dceb90dadbe6594ffd51dac436
Pedro Nakamura <nakanzaDev@gmail.com>
2025-02-19 17:35:13 -0300

    Added min/max cap + scaling

--- Assets/_Code/Script/Player/InteractableDetector.cs
+++ Assets/_Code/Script/Player/InteractableDetector.cs
@@ -8,8 +8,11 @@ public class InteractableDetector : MonoBehaviour {
     [SerializeField] private float _heightMax;
     [SerializeField] private float _heightMin;
 
+    [SerializeField, Range(0f, 1f)] private float _camCapMax;
+    [SerializeField, Range(0f, 1f)] private float _camCapMin;
+
     private List<IInteractable> _interactablesDetected = new List<IInteractable>();
-    public bool onlyHeavyObjects = false;
+    [HideInInspector] public bool onlyHeavyObjects = false;
 
     public List<IInteractable> InteractablesDetected {
         get {
@@ -24,7 +27,7 @@ public class InteractableDetector : MonoBehaviour {
     }
 
     private void Update() {
-        transform.localPosition = Mathf.Lerp(_heightMax, _heightMin, PlayerCamera.Instance.FreeLookCam.m_YAxis.Value) * Vector3.up;
+        transform.localPosition = Mathf.Lerp(_heightMax, _heightMin, ScaleRange(PlayerCamera.Instance.FreeLookCam.m_YAxis.Value)) * Vector3.up;
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -37,5 +40,20 @@ public class InteractableDetector : MonoBehaviour {
     private void OnTriggerExit(Collider other) {
         if (other.TryGetComponent(out IInteractable interactable) && _interactablesDetected.Contains(interactable)) _interactablesDetected.Remove(interactable);
     }
+
+    private float ScaleRange(float value) {
+        if (value < _camCapMin) value = _camCapMin;
+        else if (value > _camCapMax) value = _camCapMax;
+        return (value - _camCapMin) / (_camCapMax - _camCapMin);
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate() {
+        if (_camCapMax < _camCapMin) {
+            _camCapMax = _camCapMin;
+            Debug.LogWarning($"InteractableDetector max height is lower than min, setting it to min");
+        }
+    }
+#endif
 
 }
