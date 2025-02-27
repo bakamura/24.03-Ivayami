ec19408c23586ad8d2f5552b2166d4866cc32a2f
Pedro Nakamura <nakanzaDev@gmail.com>
2025-02-27 15:33:04 -0300

    Lantern properly checks for a cone

--- Assets/_Code/Script/Player/Ability/Lantern.cs
+++ Assets/_Code/Script/Player/Ability/Lantern.cs
@@ -1,37 +1,38 @@
-using UnityEngine;
-using Ivayami.Puzzle;
-using UnityEngine.Events;
 using System.Collections.Generic;
-using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Ivayami.Puzzle;
 
 namespace Ivayami.Player.Ability {
     public class Lantern : PlayerAbility {
+
+        public static UnityEvent<Vector3> OnIlluminate { get; private set; } = new UnityEvent<Vector3>();
 
         [SerializeField] private Transform _origin;
         [SerializeField] private float _lightRadius;
         [SerializeField] private float _lightFocusedRadius;
         [SerializeField] private float _lightDistance;
         [SerializeField] private LayerMask _lightableLayer;
+        [SerializeField] private LayerMask _occlusionLayer;
         private bool _enabled = false;
         private bool _focused = false;
         [SerializeField] private int _lightMaxHitNumber;
 
-        private List<ILightable> _iluminatedObjects = new List<ILightable>();
-        public static UnityEvent<Vector3> OnIlluminate { get; private set; } = new UnityEvent<Vector3>();
-
-        private RaycastHit[] _lightCastHits;
-        private Queue<ILightable> _lightCastQueue = new Queue<ILightable>();
-        private List<ILightable> _ligthCastStopIlluminating = new List<ILightable>();
-        private ILightable _ligthCastIterator;
 
         [Header("Cache")]
+
+        private HashSet<ILightable> _illuminatedObjects = new HashSet<ILightable>();
+        private HashSet<ILightable> _stopIlluminating = new HashSet<ILightable>();
+        private Collider[] _lightHits;
+        private float _coneAngleHalf;
 
         private MeshRenderer[] _meshRenderers;
 
         private void Awake() {
-            _lightCastHits = new RaycastHit[_lightMaxHitNumber];
+            _coneAngleHalf = GetComponentInChildren<Light>().spotAngle / 2f;
+            _lightHits = new Collider[_lightMaxHitNumber];
             _meshRenderers = GetComponentsInChildren<MeshRenderer>();
-            foreach(MeshRenderer meshRenderer in _meshRenderers) meshRenderer.enabled = false;
+            foreach (MeshRenderer meshRenderer in _meshRenderers) meshRenderer.enabled = false;
         }
 
         private void FixedUpdate() {
@@ -42,7 +43,11 @@ namespace Ivayami.Player.Ability {
             _enabled = !_enabled;
             foreach (MeshRenderer meshRenderer in _meshRenderers) meshRenderer.enabled = _enabled;
             PlayerAnimation.Instance.Hold(_enabled);
-            if(!_enabled) OnIlluminate.Invoke(Vector3.zero);
+            if (!_enabled) {
+                foreach (ILightable lightable in _illuminatedObjects) lightable.IluminateStop();
+                _illuminatedObjects.Clear();
+                OnIlluminate.Invoke(Vector3.zero); // Maybe remove
+            }
 
             Logger.Log(LogType.Player, $"Light {(_enabled ? "enabled" : "disabled")}");
         }
@@ -51,26 +56,27 @@ namespace Ivayami.Player.Ability {
 
         private void Illuminate() {
             OnIlluminate.Invoke(Physics.Raycast(_origin.position, _origin.forward, out RaycastHit hitLine, _lightDistance, _lightableLayer) ? hitLine.point : Vector3.zero);
-            Array.Clear(_lightCastHits, 0, _lightCastHits.Length);
-            Physics.SphereCastNonAlloc(transform.position + (transform.forward * _lightRadius), _focused ? _lightFocusedRadius : _lightRadius, -transform.forward, _lightCastHits, _lightDistance - _lightRadius, _lightableLayer);
-            foreach (RaycastHit hit in _lightCastHits) {
-                ILightable lightableComponent;
-                if (hit.transform != null) {
-                lightableComponent = hit.transform.GetComponent<ILightable>();
-                if (lightableComponent != null)_lightCastQueue.Enqueue(lightableComponent);
+
+            _stopIlluminating.Clear();
+            _stopIlluminating.UnionWith(_illuminatedObjects);
+
+            ILightable lightable;
+            for (int i = 0; i < Physics.OverlapSphereNonAlloc(_origin.position, _lightDistance, _lightHits, _lightableLayer); i++) {
+                if (_lightHits[i] != null && _lightHits[i].TryGetComponent(out lightable)) {
+                    Vector3 toTarget = _lightHits[i].transform.position - _origin.position;
+                    if (Vector3.Angle(_origin.forward, toTarget.normalized) <= _coneAngleHalf) {
+                        if (!Physics.Raycast(_origin.position, toTarget.normalized, toTarget.magnitude, _occlusionLayer)) {
+                            if (_illuminatedObjects.Add(lightable)) lightable.Iluminate();
+                            _stopIlluminating.Remove(lightable);
+                        }
+                    }
                 }
             }
-            _ligthCastStopIlluminating = _iluminatedObjects;
-            while (_lightCastQueue.Count > 0) {
-                _ligthCastIterator = _lightCastQueue.Dequeue();
-                if (_iluminatedObjects.Contains(_ligthCastIterator)) _ligthCastStopIlluminating.Remove(_ligthCastIterator);
-                else {
-                    _ligthCastIterator.Iluminate();
-                    _iluminatedObjects.Add(_ligthCastIterator);
-                }
+
+            foreach (ILightable lightableToStop in _stopIlluminating) {
+                lightableToStop.IluminateStop();
+                _illuminatedObjects.Remove(lightableToStop);
             }
-            foreach (ILightable lightable in _ligthCastStopIlluminating) lightable.IluminateStop();
-            _ligthCastStopIlluminating.Clear();
         }
 
     }
