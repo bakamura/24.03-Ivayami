3fa5a5bb619d78b134b84e4ee31ba652af185629
Pedro Nakamura <nakanzaDev@gmail.com>
2025-03-05 10:24:48 -0300

    Include focused light behaviour

--- Assets/_Code/Script/Player/Ability/Lantern.cs
+++ Assets/_Code/Script/Player/Ability/Lantern.cs
@@ -7,12 +7,13 @@ using Ivayami.Enemy;
 namespace Ivayami.Player.Ability {
     public class Lantern : PlayerAbility {
 
-        [SerializeField] private Transform _origin;
-        [SerializeField] private float _lightDistance;
+        [SerializeField] private Light _wideOrigin;
+        [SerializeField] private Light _focusedOrigin;
+        [SerializeField] private GameObject _visuals;
         [SerializeField] private LayerMask _lightableLayer;
         [SerializeField] private LayerMask _occlusionLayer;
         private bool _enabled = false;
-        private bool _focused = false;
+        private bool _focused = true; // Is inverted at start
         [SerializeField] private int _lightMaxHitNumber;
         [SerializeField, Range(0.01f, 0.5f)] private float _behaviourCheckInterval;
         private WaitForSeconds _behaviourCheckWait;
@@ -22,21 +23,21 @@ namespace Ivayami.Player.Ability {
         private HashSet<Lightable> _illuminatedObjects = new HashSet<Lightable>();
         private HashSet<Lightable> _stopIlluminating = new HashSet<Lightable>();
         private Collider[] _lightHits;
+        
+        private Transform _lightsParent;
         private float _coneAngleHalf;
+        private float _lightDistance;
 
-        private Light _light;
-        private MeshRenderer[] _meshRenderers;
 
         private const string ILLUMINATION_KEY = "Lantern";
 
         private void Awake() {
-            _coneAngleHalf = GetComponentInChildren<Light>().spotAngle / 2f;
             _lightHits = new Collider[_lightMaxHitNumber];
-            _light = GetComponentInChildren<Light>();
-            _light.enabled = false;
-            _meshRenderers = GetComponentsInChildren<MeshRenderer>();
-            foreach (MeshRenderer meshRenderer in _meshRenderers) meshRenderer.enabled = false;
+            _lightsParent = _wideOrigin.transform.parent;
+            _lightsParent.gameObject.SetActive(false);
             _behaviourCheckWait = new WaitForSeconds(_behaviourCheckInterval);
+
+            FocusChange();
         }
 
         private IEnumerator CheckInterval() {
@@ -49,8 +50,8 @@ namespace Ivayami.Player.Ability {
 
         public override void AbilityStart() {
             _enabled = !_enabled;
-            _light.enabled = _enabled;
-            foreach (MeshRenderer meshRenderer in _meshRenderers) meshRenderer.enabled = _enabled;
+            _lightsParent.gameObject.SetActive(_enabled);
+            _visuals.SetActive(enabled);
             PlayerAnimation.Instance.Hold(_enabled);
             if (_enabled) StartCoroutine(CheckInterval());
             else {
@@ -64,18 +65,18 @@ namespace Ivayami.Player.Ability {
         public override void AbilityEnd() { }
 
         private void Illuminate() {
-            if (Physics.Raycast(_origin.position, _origin.forward, out RaycastHit hitLine, _lightDistance, _lightableLayer)) LightFocuses.Instance.FocusUpdate(ILLUMINATION_KEY, hitLine.point);
+            if (Physics.Raycast(_lightsParent.position, _lightsParent.forward, out RaycastHit hitLine, _lightDistance, _lightableLayer)) LightFocuses.Instance.FocusUpdate(ILLUMINATION_KEY, hitLine.point);
             else LightFocuses.Instance.FocusRemove(ILLUMINATION_KEY);
 
             _stopIlluminating.Clear();
             _stopIlluminating.UnionWith(_illuminatedObjects);
 
             Lightable lightable;
-            for (int i = 0; i < Physics.OverlapSphereNonAlloc(_origin.position, _lightDistance, _lightHits, _lightableLayer); i++) {
+            for (int i = 0; i < Physics.OverlapSphereNonAlloc(_lightsParent.position, _lightDistance, _lightHits, _lightableLayer); i++) {
                 if (_lightHits[i] != null && _lightHits[i].TryGetComponent(out lightable)) {
-                    Vector3 toTarget = _lightHits[i].transform.position - _origin.position;
-                    if (Vector3.Angle(_origin.forward, toTarget.normalized) <= _coneAngleHalf) {
-                        if (!Physics.Raycast(_origin.position, toTarget.normalized, toTarget.magnitude, _occlusionLayer)) {
+                    Vector3 toTarget = _lightHits[i].transform.position - _lightsParent.position;
+                    if (Vector3.Angle(_lightsParent.forward, toTarget.normalized) <= _coneAngleHalf) {
+                        if (!Physics.Raycast(_lightsParent.position, toTarget.normalized, toTarget.magnitude, _occlusionLayer)) {
                             if (_illuminatedObjects.Add(lightable)) lightable.Iluminate(ILLUMINATION_KEY, true);
                             _stopIlluminating.Remove(lightable);
                         }
@@ -87,6 +88,14 @@ namespace Ivayami.Player.Ability {
                 lightableToStop.Iluminate(ILLUMINATION_KEY, false);
                 _illuminatedObjects.Remove(lightableToStop);
             }
+        }
+
+        private void FocusChange() {
+            _focused = !_focused;
+            Light light = (_focused ? _focusedOrigin : _wideOrigin);
+            _coneAngleHalf = light.spotAngle / 2f;
+            _lightDistance = light.range;
+            Debug.Log($"Cone [{(_focused ? "F" : "Unf")}ocused]:\nAngleHalf - {_coneAngleHalf}\nRange - {_lightDistance}");
         }
 
     }
