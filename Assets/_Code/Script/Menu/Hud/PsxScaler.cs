177e3d96109eb8d97fc351b6b07c17183a44002a
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2025-04-15 14:55:55 -0300

    Updates for testing

--- Assets/_Code/Script/Menu/Hud/PsxScaler.cs
+++ Assets/_Code/Script/Menu/Hud/PsxScaler.cs
@@ -4,32 +4,42 @@ using UnityEngine.Rendering;
 using System.Collections;
 using PSX;
 
-public class PsxScaler : MonoBehaviour {
+public class PsxScaler : MonoSingleton<PsxScaler> {
 
-    [SerializeField] private float _heightPixelation;
-    //[SerializeField] private float _ditheringScaleBase;
+    //[SerializeField] private float _heightPixelation;
     [SerializeField, Min(0f)] private float _windowResizeCheckInterval;
     private WaitForSeconds _windowResizeCheckWait;
 
     private Vector2Int _windowSizePrevious;
-    private Pixelation _pixelation;
-    //private Dithering _dithering;
+    // base falue takken from 1920x180 res
+    //private float _ditheringScaleBase;
+    //private Pixelation _pixelation;
+    private Dithering _dithering;
+    //private const float _baseResolutionFactor = 1920f / 1080f;
 
-    private void Awake() {
-        _pixelation = GetComponent<Volume>().profile.components.FirstOrDefault(component => component.GetType() == typeof(Pixelation)) as Pixelation;
-        //_dithering = GetComponent<Volume>().profile.components.FirstOrDefault(component => component.GetType() == typeof(Dithering)) as Dithering;
+    protected override void Awake() {
+        //_pixelation = GetComponent<Volume>().profile.components.FirstOrDefault(component => component.GetType() == typeof(Pixelation)) as Pixelation;
+        base.Awake();
+        _dithering = GetComponent<Volume>().profile.components.FirstOrDefault(component => component.GetType() == typeof(Dithering)) as Dithering;
+        //_ditheringScaleBase = _dithering.ditherScale.value;
 
-        _pixelation.widthPixelation.Override(_heightPixelation);
-        ReScale();
+        //_pixelation.widthPixelation.Override(_heightPixelation);
+        //ReScale();
 
-        _windowResizeCheckWait = new WaitForSeconds(_windowResizeCheckInterval);
-        StartCoroutine(CheckForWindowReSized());
+        //_windowResizeCheckWait = new WaitForSeconds(_windowResizeCheckInterval);
+        //StartCoroutine(CheckForWindowReSized());
     }
 
     private void ReScale() {
         _windowSizePrevious = new Vector2Int(Screen.width, Screen.height);
-        _pixelation.heightPixelation.Override(_heightPixelation * Screen.width / Screen.height);
-        //_dithering.ditherScale.Override(_ditheringScaleBase * _windowSizePrevious.y / 1080f);
+        //_pixelation.heightPixelation.Override(_heightPixelation * Screen.width / Screen.height);
+        //_dithering.ditherScale.Override(_ditheringScaleBase * _baseResolutionFactor / (Screen.width / 1080f));
+        //Debug.Log($"New scale {_dithering.ditherScale.value}");
+    }
+
+    public void ChangeDitheringScale(float value)
+    {
+        _dithering.ditherScale.Override(value);
     }
 
     private IEnumerator CheckForWindowReSized() {
