f501a92e6eb5b7098a6f6f13b64dca2782483dc3
Pedro Nakamura <nakanzaDev@gmail.com>
2025-05-15 13:01:51 -0300

    HashKeyBlocker class to wrap similar behaviour

---
+++ Assets/_Code/Script/HashKeyBlocker.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Default {
+    public class HashKeyBlocker {
+
+        private HashSet<string> _blockers = new HashSet<string>();
+        
+        public UnityEvent<bool> OnToggleChange = new UnityEvent<bool>();
+        public UnityEvent OnAllow = new UnityEvent();
+        public UnityEvent OnBlock = new UnityEvent();
+
+        public bool IsAllowed => _blockers.Count > 0;
+
+        public HashKeyBlocker() {
+            OnToggleChange.AddListener(shouldAllow => (shouldAllow ? OnAllow : OnBlock).Invoke());
+        }
+
+        public void Toggle(string key, bool shouldAllow) {
+            bool hadChange = shouldAllow ? _blockers.Remove(key) : _blockers.Add(key);
+            if (hadChange) OnToggleChange.Invoke(_blockers.Count > 0);
+            else Debug.LogWarning($"'{key}' tried to {(shouldAllow ? "un" : "")}block but was already {(shouldAllow ? "not" : "")} blocking");
+        }
+
+    }
+}
