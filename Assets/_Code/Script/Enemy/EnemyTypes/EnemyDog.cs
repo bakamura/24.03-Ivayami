48b5bdb920a9ab1251550a9446c111f33a7145bc
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2025-05-21 12:26:37 -0300

    script created + navMeshBake in scene

--- Assets/_Code/Script/Enemy/EnemyTypes/EnemyDog.cs
+++ Assets/_Code/Script/Enemy/EnemyTypes/EnemyDog.cs
@@ -1,18 +1,24 @@
+using Ivayami.Audio;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyDog : MonoBehaviour
+namespace Ivayami.Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(NavMeshAgent), typeof(BoxCollider), typeof(EnemySounds))]
+    public class EnemyDog : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
