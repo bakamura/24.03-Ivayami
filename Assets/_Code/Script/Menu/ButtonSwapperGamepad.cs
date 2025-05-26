9599f1cf99c7b5c27fd10f212ba649564c24a66e
Pedro Nakamura <nakanzaDev@gmail.com>
2025-05-26 16:36:20 -0300

    Outline of the system

---
+++ Assets/_Code/Script/Menu/ButtonSwapperGamepad.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Ivayami.Player;
+
+namespace Ivayami.UI {
+    public class ButtonSwapperGamepad : MonoBehaviour {
+
+        [SerializeField] private Button _substitutedBtn;
+        private Image _representGamepadBtn;
+
+        private void Awake() {
+            _representGamepadBtn = GetComponent<Image>();
+        }
+
+        private void Start() {
+            InputCallbacks.Instance.SubscribeToOnChangeControls(SwapBtn);
+        }
+
+        private void SwapBtn(InputCallbacks.ControlType controlType) {
+            bool isGamepad = controlType != InputCallbacks.ControlType.Keyboard;
+            _substitutedBtn?.gameObject.SetActive(!isGamepad);
+            _representGamepadBtn.enabled = isGamepad;
+        }
+
+    }
+}
