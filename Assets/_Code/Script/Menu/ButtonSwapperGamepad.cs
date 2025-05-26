147046613d46f815e642bdc789c41a648ff2ad5d
Pedro Nakamura <nakanzaDev@gmail.com>
2025-05-26 18:31:09 -0300

    Use InputIcons + Setup Main Menu

--- Assets/_Code/Script/Menu/ButtonSwapperGamepad.cs
+++ Assets/_Code/Script/Menu/ButtonSwapperGamepad.cs
@@ -6,6 +6,7 @@ namespace Ivayami.UI {
     public class ButtonSwapperGamepad : MonoBehaviour {
 
         [SerializeField] private Button _substitutedBtn;
+        [SerializeField] private InputIcons _inputIcons;
         private Image _representGamepadBtn;
 
         private void Awake() {
@@ -19,7 +20,8 @@ namespace Ivayami.UI {
         private void SwapBtn(InputCallbacks.ControlType controlType) {
             bool isGamepad = controlType != InputCallbacks.ControlType.Keyboard;
             _substitutedBtn?.gameObject.SetActive(!isGamepad);
-            _representGamepadBtn.enabled = isGamepad;
+            _representGamepadBtn.gameObject.SetActive(isGamepad);
+            if (_representGamepadBtn.gameObject.activeSelf) _representGamepadBtn.sprite = _inputIcons.Icons[(int)controlType];
         }
 
     }
