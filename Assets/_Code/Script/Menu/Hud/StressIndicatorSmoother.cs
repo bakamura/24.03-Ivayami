11ee10016114737606f94e86da4567e848bee5c7
Pedro Nakamura <nakanzaDev@gmail.com>
2025-05-29 13:44:09 -0300

    Include logic for minimal step size

--- Assets/_Code/Script/Menu/Hud/StressIndicatorSmoother.cs
+++ Assets/_Code/Script/Menu/Hud/StressIndicatorSmoother.cs
@@ -8,7 +8,7 @@ namespace Ivayami.UI {
 
         public UnityEvent<float> OnStressSmoothed { get; private set; } = new UnityEvent<float>();
         [SerializeField, Range(0f, 1f)] private float _smoothFactor;
-        [SerializeField, Min(0f)] private float _snapValue;
+        [SerializeField, Min(0f)] private float _minDisplacement;
         private float _target;
         private float _current;
         private Coroutine _coroutine;
@@ -23,13 +23,19 @@ namespace Ivayami.UI {
         }
 
         private IEnumerator SmoothToTarget() {
-            while (Mathf.Abs(_target - _current) > _snapValue) {
-                _current = Mathf.Lerp(_current, _target, _smoothFactor * Time.deltaTime);
+            float frameDisplacement;
+            float minDisplacementInFrame;
+            while (Mathf.Abs(_target - _current) > _minDisplacement) {
+                frameDisplacement = (_target - _current) * _smoothFactor * Time.deltaTime;
+                minDisplacementInFrame = _minDisplacement * Time.deltaTime;
+                if (Mathf.Abs(frameDisplacement) < minDisplacementInFrame) frameDisplacement = Mathf.Sign(frameDisplacement) * minDisplacementInFrame;
+                _current += frameDisplacement;
                 OnStressSmoothed.Invoke(_current);
 
                 yield return null;
             }
-            OnStressSmoothed.Invoke(_target);
+            _current = _target;
+            OnStressSmoothed.Invoke(_current);
             _coroutine = null;
         }
 
