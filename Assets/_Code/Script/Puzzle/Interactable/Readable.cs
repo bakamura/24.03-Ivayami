a94299f21c41c96db7f33ad97797fc65735434fe
Pedro Nakamura <nakanzaDev@gmail.com>
2025-06-06 15:40:56 -0300

    onvalidate optimization

--- Assets/_Code/Script/Puzzle/Interactable/Readable.cs
+++ Assets/_Code/Script/Puzzle/Interactable/Readable.cs
@@ -51,45 +51,50 @@ namespace Ivayami.UI {
             TitleTranslations = ResizeTextTranslations(TitleTranslations);
             ResizeToPresets();
 
-            if(false) CopyFromTables(); // Use / Adapt when making the inverse path (Tables => Asset, instead of Asset => Tables)
+#pragma warning disable CS0162 // Unreachable code detected
+            if (false) CopyFromTables(); // Use / Adapt when making the inverse path (Tables => Asset, instead of Asset => Tables)
+#pragma warning restore CS0162
         }
 
         private void AssignEmptyPresets() {
             if (PagePresets == null || PagePresets.Length == 0) PagePresets = new PagePreset[1];
-            for (int i = 0; i < PagePresets.Length; i++) if (PagePresets[i] == null)
-                    PagePresets[i] = AssetDatabase.LoadAssetAtPath<PagePreset>("Assets/_Game/Prefab/Menu/PagePresets/PagePreset_0.prefab");
+            PagePreset defaultPreset = AssetDatabase.LoadAssetAtPath<PagePreset>("Assets/_Game/Prefab/Menu/PagePresets/PagePreset_0.prefab");
+            for (int i = 0; i < PagePresets.Length; i++) if (PagePresets[i] == null) PagePresets[i] = defaultPreset;
         }
 
         private void ResizeToPresets() {
             if (_pagesContent == null) _pagesContent = new PageContent[0];
             if (_pagesContent.Length != PagePresets.Length) Array.Resize(ref _pagesContent, PagePresets.Length);
             for (int i = 0; i < _pagesContent.Length; i++) {
-                if (_pagesContent[i].textBoxes == null) _pagesContent[i].textBoxes = new PageTextBox[0];
+                _pagesContent[i].textBoxes ??= new PageTextBox[0];
                 if (_pagesContent[i].textBoxes.Length != PagePresets[i].TextBoxAmount) Array.Resize(ref _pagesContent[i].textBoxes, PagePresets[i].TextBoxAmount); // Text Boxes
                 for (int j = 0; j < _pagesContent[i].textBoxes.Length; j++) {
-                    if (_pagesContent[i].textBoxes[j].textTranslations == null) _pagesContent[i].textBoxes[j].textTranslations = new TextTranslation[0];
+                    _pagesContent[i].textBoxes[j].textTranslations ??= new TextTranslation[0];
                     _pagesContent[i].textBoxes[j].textTranslations = ResizeTextTranslations(_pagesContent[i].textBoxes[j].textTranslations); // Text Translations
                 }
-                if (_pagesContent[i].images == null) _pagesContent[i].images = new Sprite[0];
+                _pagesContent[i].images ??= new Sprite[0];
                 if (_pagesContent[i].images.Length != PagePresets[i].ImageAmount) Array.Resize(ref _pagesContent[i].images, PagePresets[i].ImageAmount); // Images
             }
         }
 
         private TextTranslation[] ResizeTextTranslations(TextTranslation[] textTranslations) {
-            int languagesCount = LocalizationSettings.AvailableLocales.Locales.Count;
+            var locales = LocalizationSettings.AvailableLocales.Locales;
+            int languagesCount = locales.Count;
 
             if (textTranslations == null || textTranslations.Length == 0) textTranslations = new TextTranslation[languagesCount];
             else if (languagesCount > 0 && textTranslations.Length != languagesCount) Array.Resize(ref textTranslations, languagesCount);
             else return textTranslations;
 
-            for (int i = 0; i < textTranslations.Length; i++) textTranslations[i].language = LocalizationSettings.AvailableLocales.Locales[i].LocaleName;
+            for (int i = 0; i < textTranslations.Length; i++) textTranslations[i].language = locales[i].LocaleName;
             return textTranslations;
         }
 
         private void CopyFromTables() {
-            for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; i++)
-                _pagesContent[0].textBoxes[0].textTranslations[i].text =
-                    LocalizationSettings.StringDatabase.GetLocalizedString("Items", $"{name}/Description", LocalizationSettings.AvailableLocales.Locales[i]);
+            var locales = LocalizationSettings.AvailableLocales.Locales;
+            for (int i = 0; i < locales.Count; i++) {
+                TitleTranslations[i].text = LocalizationSettings.StringDatabase.GetLocalizedString("Items", TitleLocalizationString, locales[i]);
+                _pagesContent[0].textBoxes[0].textTranslations[i].text = LocalizationSettings.StringDatabase.GetLocalizedString("Items", $"{name}/Description", locales[i]);
+            }
         }
 #endif
 
