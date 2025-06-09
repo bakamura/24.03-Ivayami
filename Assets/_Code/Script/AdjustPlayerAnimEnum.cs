3d4bc9cf0aab91dd942a7b419c78522ae2554a71
Pedro Nakamura <nakanzaDev@gmail.com>
2025-06-09 14:37:14 -0300

    Use reflection in the migration tool

--- Assets/_Code/Script/AdjustPlayerAnimEnum.cs
+++ Assets/_Code/Script/AdjustPlayerAnimEnum.cs
@@ -1,37 +1,44 @@
 #if UNITY_EDITOR
+using System.Reflection;
 using UnityEditor;
-using UnityEngine;
 using UnityEditor.SceneManagement;
+using UnityEngine;
 using Ivayami.Player;
 
-public class AdjustPlayerAnimEnum {
-    [MenuItem("Tools/Migrate Enum Values")]
-    static void Migrate() {
-        var scenes = AssetDatabase.FindAssets("t:Scene");
-        foreach (var guid in scenes) {
-            var path = AssetDatabase.GUIDToAssetPath(guid);
-            if (!path.StartsWith("Assets/_Game/Scene/Build")) continue;
-            var scene = EditorSceneManager.OpenScene(path);
+namespace Ivayami.debug {
+    public class AdjustPlayerAnimEnum {
 
-            var allObjs = Object.FindObjectsOfType<MonoBehaviour>(true);
-            foreach (var obj in allObjs) {
-                var so = new SerializedObject(obj);
-                var prop = so.GetIterator();
-                while (prop.NextVisible(true)) {
-                    if (prop.propertyType == SerializedPropertyType.Enum && prop.type == nameof(PlayerActions.InteractAnimation) && prop.enumValueIndex > 0) // new one is at 1
-                    {
-                        prop.enumValueIndex += 1; // so everything above 1 should be incresead 1
-                        so.ApplyModifiedProperties();
+        [MenuItem("Tools/Migrate Enum Values")]
+        static void Migrate() {
+            var scenes = AssetDatabase.FindAssets("t:Scene");
+            foreach (var guid in scenes) {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                if (!path.StartsWith("Assets/_Game/Scene/Build")) continue;
+                var scene = EditorSceneManager.OpenScene(path);
+
+                var allObjs = Object.FindObjectsOfType<MonoBehaviour>(true);
+                foreach (var obj in allObjs) {
+                    var so = new SerializedObject((Object)obj);
+                    var prop = so.GetIterator();
+                    while (prop.NextVisible(true)) {
+                        var type = obj.GetType();
+                        var field = type.GetField(prop.name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                        if (field != null && field.FieldType == typeof(PlayerActions.InteractAnimation) && prop.enumValueIndex > 0) {
+                            Debug.Log($"{prop.name}");
+                            prop.enumValueIndex += 1;
+                            so.ApplyModifiedProperties();
+                        }
                     }
                 }
+
+                EditorSceneManager.MarkSceneDirty(scene);
+                EditorSceneManager.SaveScene(scene);
             }
 
-            EditorSceneManager.MarkSceneDirty(scene);
-            EditorSceneManager.SaveScene(scene);
+            AssetDatabase.SaveAssets();
+            Debug.Log("Enum migration complete.");
         }
 
-        AssetDatabase.SaveAssets();
-        Debug.Log("Enum migration complete.");
     }
 }
 #endif
